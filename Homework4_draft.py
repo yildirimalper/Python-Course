a31540f6654a326c0376ec8581d7de0f73a15fd9
Alper Yıldırım <alperyildirim17@ku.edu.tr>
2021-05-15 14:04:25 +0300

    homework 4 draft file

--- Homework4_draft.py
+++ Homework4_draft.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+from scipy import sparse
 
 imm = pd.read_csv("../Homework4/immSurvey.csv")
 X, y = imm.text, imm.sentiment
